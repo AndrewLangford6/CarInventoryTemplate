d8dcec29dfd53a095dce7971bf8466f5c06c2e6c
Andrew Langford <Andrew.Langford.6@gmail.com>
2020-02-14 13:07:25 -0500

    Add project files.

---
+++ CarInventory/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarInventory
+{
+    class Car
+    {
+        public string year, make, colour, milage;
+
+            public Car(string _year, string _make, string _colour, string _milage)
+        {
+            year = _year;
+            make = _make;
+            colour = _colour;
+            milage = _milage;
+        }
+    }
+}
---
+++ CarInventory/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CarInventory
+{
+    public partial class Form1 : Form
+    {
+
+        List<Car> carsList = new List<Car>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string year, make, colour, milage;
+            year = yearInput.Text;
+            make = makeInput.Text;
+            colour = colourInput.Text;
+            milage = mileageInput.Text;
+
+            Car c = new Car(year, make, colour, milage);
+            carsList.Add(c);
+
+            outputLabel.Text = "";
+
+            /*
+            for (int i = 0; i < carsList.Count; i++)
+            {
+                outputLabel.Text += carsList[i].year + " " + carsList[i].make + " " + carsList[i].colour + " " + carsList[i].milage + "km \n";
+            }
+            */
+            foreach(Car car in carsList)
+            {
+                outputLabel.Text += car.year + " " + car.make + " " + car.colour + " " + car.milage + "km \n";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
